0c87dbb0a17292bbf040e37be30b71ef44c12130
Darwin <risser.darwin@gmail.com>
2019-01-29 19:55:57 -0600

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/users', (request,response,next) => {
+    response.send("hi")
+});
+   
+app.post('/users', (request,response,next) => {
+    users.push(request.body)
+    response.send("user saved")
+});
+
+app.get('/users/:id', (request,response,next) => {
+    response.json({theidfromthepath:request.params.id})
+});
+   
+
+app.listen(3002, (err) => {
+  if (err) {
+    return console.log("Error", err);
+  }
+  console.log("Web server is now listening for messages", err);
+});
