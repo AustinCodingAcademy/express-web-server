28007ff36de17f0648fbc08b8bb6d0d27cca2323
Abby Gottlich <abby.gottlich@gmail.com>
2019-01-30 20:08:33 -0600

    GET, POST, PUT, DELETE working

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// GET will display in the browser
+// POST will display in Postman
+// PUT will display in Postman
+// DELETE will display in Postman
+// must GET something before you can see it POSTed to the browser
+
 const express = require("express");
 const bodyParser = require("body-parser");
+let users = require("./state").users;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,25 +14,39 @@ app.use(bodyParser.json());
 app.get('/users', (request, response, next) => {
     // lets you define what's in the GET object
     // '/users' is your key; the callback is where the value comes from
-    response.send("hi")
+    response.send(users)
 });
 
 app.post('/users', (request, response, next) => {
     // lets you define what's in the POST object
     // '/users' is your key; the callback is where the value comes from
-    response.send("hi from posting")
+    users.push({
+        "id": 6,
+        "name": "abby",
+        "occupation": "coder",
+        "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+    })
+    response.send(users)
 });
 
 app.get('/users/:id', (request, response, next) => {
     // request.params is part of express
     // we came up with id
-    response.json({ theidfromthepath: request.params.id })
+    response.json(users[request.params.id - 1])
 });
 
 app.put('/users/1', (request, response, next) => {
     // lets you define what's in the PUT object
     // '/users' is your key; the callback is where the value comes from
-    response.send("PUT PUT PUT")
+    users[0].name = "Todd";
+    response.send(users[0].name)
+});
+
+app.delete('/users/1', (request, response, next) => {
+    // lets you define what's in the DELETE object
+    // '/users' is your key; the callback is where the value comes from
+    users.pop()
+    response.send(users)
 });
 
 app.listen(3002, (err) => {
