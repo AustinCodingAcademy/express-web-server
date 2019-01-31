d2d178c93482f656c5c1f6674c6f9eb6f2f206df
Abby Gottlich <abby.gottlich@gmail.com>
2019-01-30 20:52:45 -0600

    delete function working!!!!!

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ let users = require("./state").users;
 const app = express();
 app.use(bodyParser.json());
 
+// displaying users array in browser
 app.get('/users', (request, response, next) => {
     // lets you define what's in the GET object
     // '/users' is your key; the callback is where the value comes from
     response.send(users)
 });
 
+// adding a new user to the users array in Postman and in browser
 app.post('/users', (request, response, next) => {
     // lets you define what's in the POST object
     // '/users' is your key; the callback is where the value comes from
@@ -29,24 +31,31 @@ app.post('/users', (request, response, next) => {
     response.send(users)
 });
 
+// getting a specific user to display in the browser
 app.get('/users/:id', (request, response, next) => {
     // request.params is part of express
     // we came up with id
     response.json(users[request.params.id - 1])
 });
 
-app.put('/users/1', (request, response, next) => {
+// changing a key value of a specific user
+app.put('/users/:id', (request, response, next) => {
     // lets you define what's in the PUT object
     // '/users' is your key; the callback is where the value comes from
-    users[0].name = "Todd";
-    response.send(users[0].name)
+    users[request.params.id - 1].name = "Todd";
+    response.json(users[request.params.id - 1])
 });
 
-app.delete('/users/1', (request, response, next) => {
+// deleting the last user in the array
+app.delete('/users/:id', (request, response, next) => {
     // lets you define what's in the DELETE object
     // '/users' is your key; the callback is where the value comes from
-    users.pop()
-    response.send(users)
+    users[request.params.id - 1].isActive = false;
+    response.send("deleted")
+    if (users[request.params.id - 1].isActive === false) {
+        users.splice(request.params.id - 1, 1)
+    }
+    console.log(users)
 });
 
 app.listen(3002, (err) => {
